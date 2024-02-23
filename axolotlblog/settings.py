08e3d0d9738459a5f82b6bcfb1d4e90a0f2d0496
Alma Isaksson <isakvent@outlook.com>
2024-02-23 19:32:31 +0000

    deployment commit

--- axolotlblog/settings.py
+++ axolotlblog/settings.py
@@ -31,7 +31,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+# summernote wouldn't work on deployment due to security
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['axolotl-blog-66ea1f84b437.herokuapp.com', 'localhost']
 
